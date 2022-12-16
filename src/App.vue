5a97f4667d3c6141e8ab32d38909882e7a814124
Eliza Erzanukaeva <akribophobia@outlook.com>
2022-12-16 14:01:18 +0400

    feat(Card): create card component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
 <template>
-  <nav>
-   
-  </nav>
+  <div class="flex-container flex-container_column">
+    <div class="flex-container">
+      <aside>
+        <card />
+      </aside>
+    </div>
+    
+  </div>
+
+
 
 </template>
 
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
 <style lang="scss">
 #app {
+  padding: 0;
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #262626;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-nav {
-  padding: 30px;
+aside {
+  width: 30%;
+  max-width: 236px;
+  height: 500px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.flex-container {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &_column{
+    flex-direction: column;
   }
+
 }
+
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,99 @@
+<template>
+    <section class="card">
+        <slot name="card__header"></slot>
+        <img class="card__img" src="@/assets/aside-image.png">
+        <div class="card__body">
+            <h4 class="card__title text"></h4>
+            <p class="card__text text"></p>
+            <p class="card__text text"></p>
+            <p class="card__text text"></p>
+            <p class="card__text text"></p>
+            <p class="card__text text"></p>
+
+        </div>
+        <footer class="card__footer text"></footer>
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'card',
+    props: {
+    },
+
+}
+</script>
+
+<style lang="scss">
+.text {
+    background: linear-gradient(90deg, #3C3C3C 0%, #444444 51.04%, #333333 100%);
+    border-radius: 4px;
+    height: 10px;
+    width: 100%;
+    margin: 0.6rem 0;
+}
+
+.card {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background: #262626;
+    border: 1px solid #4D4D4D;
+    border-radius: 12px;
+    max-width: 236px;
+    height: inherit;
+
+    &__img {
+        align-self: center;
+        margin: 1rem 0 0.5rem;
+        width: 208px;
+        height: 240px;
+        backdrop-filter: blur(6px);
+        border-radius: 8px;
+    }
+
+    &__title {
+        max-width: 190px;
+        height: 26px;
+        border-radius: 8px;
+    }
+
+    &__footer {
+        width: 80px;
+        margin: 1rem 0;
+    }
+
+    p {
+       
+        &:first-of-type {
+            width: 155px;
+        }
+
+        &:nth-of-type(3) {
+            width: 170px;
+        }
+
+        &:nth-of-type(4) {
+            width: 160px;
+        }
+
+        &:nth-of-type(5) {
+            width: 140px;
+        }
+    }
+
+    &__body {
+        width: 80%;
+        min-width: max-content;
+        // height: max-content;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+}
+</style>
